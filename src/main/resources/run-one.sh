e460b82fd59f3eee048e7fcc085e2d42f4b85cbe
Martin Biely <martin.biely@epfl.ch>
2012-07-18 16:22:50 +0200

    move to testing also for config

--- src/main/resources/run-one.sh
+++ src/main/resources/run-one.sh
@@ -8,13 +8,17 @@ OPTS="-server"
 
 case $1 in 
     server)
-	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.netty.ServerMain ;;
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.netty.ServerMain ;;
     actor)
-	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.netty.akkabridge.ServerMain ;;
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.netty.akkabridge.ServerMain ;;
     client) 
-	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.netty.ClientMain $2 $3 ;;
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.netty.ClientMain $2 $3 ;;
     akkaclient)
-	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.akkatest.ClientMain $2 $3 ;;
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.akkatest.ClientMain $2 $3 ;;
     akkaserver)
-	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.akkatest.ServerMain ;;
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.akkatest.ServerMain ;;
+    kryonetserver)
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.kryonet.ServerMain ;;
+    kryonetclient)
+	time java $OPTS -cp '@@DIR@@:@@DIR@@/@@JAR@@:@@DIR@@/deps/*:' ch.epfl.lsr.testing.kryonet.ClientMain $2 $3 ;;
 esac
